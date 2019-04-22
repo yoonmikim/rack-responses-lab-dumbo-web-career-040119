ae1f7214ff450da227b38f0cff8bc5853babaa22
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-22 13:37:10 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,13 @@
+class Application 
+  def call(env)
+    resp = Rack::Response.new 
+    
+    if env <= 12
+      resp.write "Good Morning"
+    else
+      resp.write "Good Afternoon"
+    end
+    resp.finish
+  end
+  
+end
